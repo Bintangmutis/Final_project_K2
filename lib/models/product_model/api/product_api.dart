86cd4a089282835257df988fdfd6745cab49b64d
AfkrNaqy <naqy22afka@gmail.com>
2022-12-06 20:44:23 +0700

    fitur search

--- lib/models/product_model/api/product_api.dart
+++ lib/models/product_model/api/product_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:final_project_kel_2/models/product_model/productmodel.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -49,5 +50,40 @@ class ProductApi {
     return listOfProducts
         .where((element) => element.category.name.toLowerCase().contains(value))
         .toList();
+  }
+
+  static Future<List<ProductModel>> searchProduct(String query) async {
+    Uri url = Uri.parse("https://api1.sib3.nurulfikri.com/api/barang");
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String token = prefs.getString('token')!;
+
+    final response = await http.get(
+      url,
+      headers: ({
+        "Accept": "application/json",
+        "Authorization": "Bearer $token"
+      }),
+    );
+
+    final responseBody = json.decode(response.body)['data'];
+
+    if (response.statusCode == 200) {
+      final List listOfProduct = (responseBody as List);
+      return listOfProduct
+          .map((json) => ProductModel.fromJson(json))
+          .where((product) {
+        final nameProduct = product.name.toLowerCase();
+        final categoryProduct = product.category.name.toLowerCase();
+        final searchText = query.toLowerCase();
+
+        return nameProduct.contains(searchText) ||
+            categoryProduct.contains(searchText);
+        ;
+      }).toList();
+    } else {
+      throw Exception();
+    }
   }
 }
